43c64d87fe45240011c5574a3adb8c65196ed379
Andrii <andriikshnr@gmail.com>
2025-04-09 17:52:08 +0300

    Add Railway host and CSRF trusted origin

--- videostore/settings.py
+++ videostore/settings.py
@@ -16,8 +16,15 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'web-production-7ae3.up.railway.app',
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-7ae3.up.railway.app'
+]
 
 # Application definition
 
